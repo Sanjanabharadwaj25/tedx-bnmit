1d789195ba71e069c5ea1e89d2c9d3661c6492af
Sanjana Bharadwaj <sanjana2002251@gmail.com>
2023-08-03 18:50:04 +0530

    Cursor Animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React from "react";
 import Landing from "./pages/Landing"
+import Cursor from "./components/Cursor";
+import "./components/Cursor.module.css"; // Import the CSS file for cursor styles
+
+
 export default function App() {
   return (
+    <>
     <Landing/>
+    <Cursor/>
+    
+    </>
   )
 }
---
+++ src/components/Cursor.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import "./Cursor.module.css"; // Import the CSS file for cursor styles
+
+const Cursor = () => {
+  useEffect(() => {
+    const coords = { x: 0, y: 0 };
+    const circles = document.querySelectorAll(".circle");
+
+    circles.forEach(function (circle, index) {
+      circle.x = 0;
+      circle.y = 0;
+    });
+
+    window.addEventListener("mousemove", function (e) {
+      coords.x = e.clientX;
+      coords.y = e.clientY;
+    });
+
+    function animateCircles() {
+      let x = coords.x;
+      let y = coords.y;
+
+      circles.forEach(function (circle, index) {
+        circle.style.left = x - 12 + "px";
+        circle.style.top = y - 12 + "px";
+
+        circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+      });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+  }, []);
+
+  return (
+    <div className="cursor-name">
+      {/* Your circle elements */}
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  );
+};
+
+export default Cursor;
