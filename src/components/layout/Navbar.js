5cc828696942faf090f597baafccbb7636fda372
Sanjana Bharadwaj <sanjana2002251@gmail.com>
2023-08-11 16:41:57 +0530

    Changing navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../../assets/logo.png"
 function Navbar(){
- return <header class="text-gray-600 body-font">
+ return <header class="text-gray-600 body-font navbar">
  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
    <div class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
     <img src={Logo} alt="Logo"/>
